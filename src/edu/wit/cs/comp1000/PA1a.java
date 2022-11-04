196523edd0ad9c1ecada2b2cc98cf4af1f479e3f
321056 <321056@664-161481-LAB.schools.mybps.org>
2022-11-04 11:19:14 -0400

    New code

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -7,6 +7,7 @@ public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
+		System.out.println(" Hello Peanut Butter! ");
 		// TODO: write your code here
 	}
 
