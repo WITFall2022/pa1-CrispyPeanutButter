1a3ea9a1d2242d3eb1a7183b439cb1cb1ede53f6
321056 <321056@664-161481-LAB.schools.mybps.org>
2022-12-15 11:18:10 -0500

    I submitted the code

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -2,13 +2,31 @@ package edu.wit.cs.comp1000;
 
 import java.util.Scanner;
 
-// TODO: PA1 
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		System.out.println(" Hello Peanut Butter! ");
 		// TODO: write your code here
+
+		Scanner input = new Scanner(System.in);
+		
+		int yards;
+		int feet;
+		int inches;
+		
+		System.out.printf("Enter amount of yards as a whole number:");
+		yards = input.nextInt();
+		
+		System.out.printf("Enter amount of feet as a whole number:");
+		feet = input.nextInt();
+		
+		System.out.printf("Enter amount of inches as a whole number:");
+		inches = input.nextInt();
+		
+		int ans;
+		ans = feet*12+yards*36+inches;
+		System.out.print(ans);
+	
 	}
 
 }
